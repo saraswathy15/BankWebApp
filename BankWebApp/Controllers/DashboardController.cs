70d9da53ecee784d3359b6e26b57665c5f5e53e2
saraswathy15 <charusrinivasan15@gmail.com>
2024-03-13 12:49:53 +0530

    Add project files.

---
+++ BankWebApp/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankWebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index(string username)
+        {
+            ViewBag.Username = username;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            // Add logic for logout
+            // Redirect to Login page
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ BankWebApp/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+            public IActionResult Index()
+            {
+                return View();
+            }
+            [HttpGet]
+            public IActionResult Login()
+            {
+                return View();
+            }
+            [HttpPost]
+            public IActionResult Login(string username, string password)
+            {
+            
+                return RedirectToAction("Index", "Dashboard", new { username = username });
+            }
+        }
+}
